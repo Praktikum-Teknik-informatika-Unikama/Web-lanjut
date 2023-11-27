db082e46f4cd6ecdd46dd2cf9dd7623f42e8e384
aufall02 <aufaluzumaki@gmail.com>
2023-11-27 23:59:57 +0700

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,20 @@
 
 @section('content')
     <div class="container">
-        ini tampilan welcome
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama</label>
+                <input type="text" class="form-control" id="nama" placeholder="masukkan nama anda">
+            </div>
+            <div class="mb-3">
+                <label for="nim" class="form-label">Nomer Induk Mahasiswa</label>
+                <input type="text" class="form-control" id="nim" placeholder="masukkan nim anda">
+            </div>
+            <div class="mb-3">
+                <label for="prodi" class="form-label">Program Studi</label>
+                <input type="text" class="form-control" id="prodi" placeholder="masukkan prodi anda">
+            </div>
+            <button class="btn btn-primary" type="submit">Button</button>
+        </form>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\HalloController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
