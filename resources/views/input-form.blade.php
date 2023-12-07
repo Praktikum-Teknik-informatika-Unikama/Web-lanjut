6d2b5344d3c42e8982ac89688a1d855f0e0aef50
aufall02 <aufaluzumaki@gmail.com>
2023-12-07 21:46:14 +0700

    .

--- resources/views/input-form.blade.php
+++ resources/views/input-form.blade.php
@@ -1,36 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    @if (!$mahasiswa)
-        <h2>Input Data Diri</h2>
-        <div>
-            <form action="{{ route('create') }}" method="post">
-                @csrf
-                <label for="nama">Masukkan nama anda : </label>
-                <input type="text" id="nama" name="nama" placeholder="ex. jhon"> <br><br>
-                <label for="nim">Masukkan nomer induk mahasiswa anda : </label>
-                <input type="text" id="nim" name="nim"placeholder="ex. 2134"> <br><br>
-                <label for="prodi">Masukkan program studi anda : </label>
-                <input type="text" id="prodi" name="prodi"placeholder="ex. teknik informatika"><br><br>
-                <input type="submit" value="submit">
-            </form>
-        </div>
-    @else
-        <h2>Update Data Diri</h2>
-        <div>
-            <form action="/update-mahasiswa/{{$mahasiswa->id}}" method="POST">
-                @csrf
-                <input name="_method" type="hidden" value="PUT">
-                <label for="nama">Nama : </label>
-                <input type="text" id="nama" name="nama" value="{{$mahasiswa->nama}}"> <br><br>
-                <label for="nim">Nomer induk mahasiswa : </label>
-                <input type="text" id="nim" name="nim" value="{{$mahasiswa->nim}}"> <br><br>
-                <label for="prodi">Program studi : </label>
-                <input type="text" id="prodi" name="prodi" value="{{$mahasiswa->prodi}}"><br><br>
-                <button type="submit">submit</button>
-            </form>
-        </div>
-    @endif
+    {{ $mahasiswa != null ? echo '<h2>Update Data Diri</h2>' : echo '<h2>Input Data Diri</h2>' }}
+    @php
+        $action = $mahasiswa ? '/add-mahasiswa' : '/update-mahasiswa/{{ $mahasiswa->id }}';
+    @endphp
 
 
+    <div>
+        <form action="{{ $action }}" method="post">
+            @csrf
+            <label for="nama">Masukkan nama anda : </label>
+            <input type="text" id="nama" name="nama" value="{{ $mahasiswa ? $mahasiswa->nama : '' }}"
+                placeholder="ex. jhon"> <br><br>
+            <label for="nim">Masukkan nomer induk mahasiswa anda : </label>
+            <input type="text" id="nim" name="nim" value="{{ $mahasiswa ? $mahasiswa->nim : '' }}"
+                placeholder="ex. 2134"> <br><br>
+            <label for="prodi">Masukkan program studi anda : </label>
+            <input type="text" id="prodi" name="prodi" value="{{ $mahasiswa ? $mahasiswa->prodi : '' }}"
+                placeholder="ex. teknik informatika"><br><br>
+            <input type="submit" value="submit">
+        </form>
+    </div>
 @endsection
